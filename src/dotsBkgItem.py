e79bb880e88e64a13454bfbff2c59eaed5bbc596
melt <mel.tearle@gmail.com>
2023-12-01 21:28:11 -0800

    Missing Files Restored

---
+++ src/dotsBkgItem.py
@@ -0,0 +1,327 @@
+
+import os
+
+
+from PyQt6.QtCore       import Qt, QPointF, QPropertyAnimation, pyqtSlot
+from PyQt6.QtGui        import QImage, QPixmap
+from PyQt6.QtWidgets    import QGraphicsPixmapItem
+                        
+from dotsShared         import common
+from dotsSideGig        import MsgBox, point
+from dotsBkgWorks       import BkgWorks
+
+import dotsAnimation    as Anime
+
+### ---------------------- dotsBkgItem ---------------------                   
+''' See StartHere.md for a detailed explanation on scrolling backgrounds '''  
+### --------------------------------------------------------
+
+screentime = {  ## based on a 1280X640 .jpg under .5MB for 16:9 background
+        ##   first, next-left, next-right --- there are always two backgrounds once started
+    '1080':  (10.0,  17.35,  17.40),     ## 1440px actual size when scaled 1280X640 for 16:9
+    '1280':  (10.0,  18.75,  18.85),  
+    '1215':  (10.0,  17.35,  17.4),     ## 1620px actual size when scaled 1280X640 for 16:9
+    '1440':  (10.0,  18.7,   18.85),
+    '1296':  (10.0,  17.35,  17.4),     ## 1728px actual size when scaled 1280X640 for 16:9
+    '1536':  (10.0,  18.65,  18.60),  
+    '1102':  (10.0,  20.9,    0.0),
+    '900':   (10.0,  23.2,    0.0),
+    '912':   (10.0,  20.9,   0.0),
+}
+
+moretimes = {   ## based on a 1080X640 .jpg under .5MB for 3:2 background
+    '1080':  (10.0,   18.88, 19.05),   ## 1215px actual size when scaled 1080X640 for 3:2
+    '1215':  (10.0,   18.88, 18.88),   ## 1367px actual size when scaled 1080X640 for 3:2
+    '1296':  (10.0,   18.88, 18.88),   ## 1458px actual size when scaled 1080X640 for 3:2
+    '900':   (10.0,   21.4,   0.0),    ## 1013px actual size when scaled 640X1080 for 2:3
+}
+
+showtime = {  ## trigger to add a new background based on number of pixels remaining in runway
+    'snakes':   15,  ## also used by vertical 
+    'left':     11, 
+    'right':    13,  
+    'vertical': 15,  ## trying this out 
+}
+       
+ScaleKeys  = ("<",">")
+        
+### --------------------------------------------------------
+class BkgItem(QGraphicsPixmapItem):  ## background
+### --------------------------------------------------------
+    def __init__(self, fileName, canvas, z=common['bkgZ']):
+        super().__init__()
+
+        self.canvas   = canvas
+        self.dots     = self.canvas.dots
+        self.scene    = self.canvas.scene
+        self.bkgMaker = self.canvas.bkgMaker
+        
+        self.bkgWorks = BkgWorks(self)
+      
+        self.ViewW = common['ViewW']
+        self.ViewH = common['ViewH']
+
+        self.type = 'bkg'
+        self.fileName = fileName  
+              
+        self.setZValue(z)      
+        self.init()
+  
+### --------------------------------------------------------   
+    def init(self):    
+        self.scrollable = False 
+        self.imgFile = None    
+        try:   ## --- sets if scrollable --- ##
+            if not self.dots.Vertical: 
+                self.bkgWorks.setWidthHeight(QImage(self.fileName)) 
+            else:
+                self.bkgWorks.setVertical(QImage(self.fileName))
+        except Exception:
+            MsgBox('error on loading: ' + self.fileName)
+            return
+                 
+        self.setTransformationMode(Qt.TransformationMode.SmoothTransformation)
+                                            
+        self.id = 0  ## not used except for conisistency          
+        self.flopped = False
+        self.locked = False
+                    
+        self.width  = self.imgFile.width()
+        self.height = self.imgFile.height() 
+            
+        if not self.dots.Vertical: 
+            self.setMirrored(False)
+                                                 
+        self.opacity  = 1.0
+        self.rotation = 0
+        self.scale    = 1
+
+        self.x, self.y = 0.0, 0.0
+        
+        self.tag = ''  
+        self.anime = None
+        self.matte = None
+        
+        self.addedScroller = False
+           
+        self.direction = ''  ## direction of scroll 
+        self.factor    = self.bkgMaker.factor    ## sets default    
+        self.mirroring = self.bkgMaker.mirroring ## sets default - false equals continuous
+        
+        ## number of pixels before the runway ends and then it's showtime
+        self.showtime = 0  
+        self.runway = 0  ## what's not visible
+           
+        self.setShowTime()
+        self.setRunWay()  
+    
+        self.setPixmap(QPixmap.fromImage(self.imgFile)) 
+                              
+        self.ratio = self.height/9
+        self.ratio = int(self.width/self.ratio)
+                    
+        self.setFlag(QGraphicsPixmapItem.GraphicsItemFlag.ItemSendsScenePositionChanges, False)                                 
+        self.setFlag(QGraphicsPixmapItem.GraphicsItemFlag.ItemIsMovable, True) 
+          
+        self.key = ''
+        
+        self.dragCnt = 0
+        self.save = QPointF()  
+              
+### -------------------------------------------------------- 
+    @pyqtSlot(str)
+    def setPixKeys(self, key):
+        self.key = key 
+        if key in ScaleKeys:
+            self.scaleThis(key)  
+                               
+    def mousePressEvent(self, e):
+        if not self.canvas.pathMakerOn:       
+            if e.button() == Qt.MouseButton.RightButton:
+                self.bkgMaker.addWidget(self)                    
+            elif self.canvas.key == 'del':    
+                self.delete()          
+            elif self.canvas.key == '/':  ## to back
+                self.bkgMaker.back(self)     
+            elif self.canvas.key == 'shift':  ## flop it
+                self.flopped = not self.flopped 
+                self.setMirrored(self.flopped)     
+            elif self.canvas.key in ('enter','return'):  ## to front  
+                self.bkgMaker.front(self)             
+            elif self.key == ',':
+                self.bkgMaker.backOne(self)  
+            elif self.key == '.':
+                self.bkgMaker.upOne(self)  
+            elif self.key == 'space':  ## show background tag
+                self.bkgWorks.tagBkg(self, e.scenePos())
+            self.initX, self.initY = self.x, self.y  
+            self.dragCnt = self.mapToScene(e.pos())
+        e.accept()     
+
+    def mouseMoveEvent(self, e):
+        self.updateXY(self.mapToScene(e.pos()))
+        self.setPos(self.x, self.y)
+        e.accept()
+
+    def mouseReleaseEvent(self, e):
+        if not self.canvas.pathMakerOn: 
+            self.canvas.key = ''
+        self.updateXY(self.mapToScene(e.pos()))
+        self.setPos(self.x, self.y)
+        e.accept()
+        
+### --------------------------------------------------------              
+    def setRunWay(self):      
+        if not self.dots.Vertical:             
+            self.runway = int(common['ViewW'] - self.width)  ## pixels outside of view
+        else:
+            self.runway = int(common['ViewH'] - self.height) 
+              
+    def setShowTime(self):  ## updated
+        if self.direction == 'left':   
+            self.showtime = showtime['left']
+        elif self.direction == 'right':  
+            self.showtime = showtime['right']      
+        ## snakes need more time - the rest vary to build and position and comes before vertical 
+        elif 'snakes' in self.fileName and self.direction != 'vertical':
+            self.showtime = showtime['snakes']   
+        elif self.direction == 'vertical':  ## see vertical in bkgWorks - there's a kludge
+            self.showtime = showtime['vertical']   
+                            
+### -------------------------------------------------------- 
+    def itemChange(self, change, value):
+        if change == QGraphicsPixmapItem.GraphicsItemChange.ItemScenePositionHasChanged: 
+            if self.direction == '':
+                return      
+              
+            ## 'first' has already set its setScrollerPath                 
+            elif self.addedScroller == False:  ## check if its showtime                      
+                if self.direction  == 'left'  and int(value.x()-self.runway) <= self.showtime or\
+                    self.direction == 'right' and int(value.x()) >= -self.showtime or\
+                    self.direction == 'vertical' and int(value.y())-self.runway <= self.showtime:  
+                    self.addNextScroller() 
+                    self.addedScroller = True  
+                                                 
+            elif self.addedScroller == True:  ## test if nolonger in view
+                if self.direction   == 'left'  and abs((value.x())) >= self.width or \
+                    self.direction  == 'right' and abs(int(value.x())) >= common['ViewW'] or \
+                    self.direction  == 'vertical' and int(value.y()) >= self.height:
+                    self.anime.stop()
+                    self.bkgMaker.delScroller(self)   
+                                                    
+        return super(QGraphicsPixmapItem, self).itemChange(change, value)
+ 
+### --------------------------------------------------------                       
+    def addNextScroller(self):  ## add and scroll the next background           
+        item = BkgItem(self.fileName, self.canvas, common['bkgZ']) 
+                   
+        if self.mirroring == False:  ## continuous
+            item.setMirrored(False)
+        elif self.ratio <= 27 or self.dots.Vertical == False:  ## 27:9 = 3:1 
+            item.setMirrored(False) if self.flopped else item.setMirrored(True) 
+        else:
+            item.setMirrored(False)
+                               
+        item.tag = 'scroller'
+        item.setZValue(self.zValue())  
+        item.showtime = self.showtime   
+             
+        self.bkgWorks.restoreFromTrackers(item, 'addnxt')  ## gets set to default   
+        item.anime = self.setScrollerPath(item, 'next')  ## txt for debugging      
+  
+        item.setFlag(QGraphicsPixmapItem.GraphicsItemFlag.ItemSendsScenePositionChanges, True)         
+        self.scene.addItem(item)   
+           
+        item.anime.start()
+                               
+### --------------------------------------------------------   
+    ''' sets scroller animation - runs directly from demos as well '''
+### -------------------------------------------------------- 
+    def setScrollerPath(self, bkg, which, scr=''): ## which rate to use, 1 == rate[0]  
+        if bkg.direction == '':   
+            return      
+
+        path = self.setPath(bkg)         
+        if path == None:
+            MsgBox('error setting scroller path...')
+            return
+                
+        if bkg.direction in ('left', 'right') and bkg.width < common['ViewW'] or\
+            bkg.direction == 'vertical' and bkg.height < common['ViewH']:
+            self.bkgWorks.notScrollable()  
+            return 
+ 
+        if bkg.direction == 'right':  
+            bkg.setPos(QPointF(bkg.runway, 0))  ## if not yet done
+        elif bkg.direction == 'left':  
+            bkg.setPos(QPointF())
+        else:
+            bkg.setPos(QPointF(0.0, float(bkg.runway)))
+                       
+        if common['Screen'] ==  '1080' and bkg.width < 1315:  ## 1080X640 3:2 format else use 16:9
+            rate = moretimes[common['Screen']]
+        elif common['Screen'] == '1215' and bkg.width < 1467:
+            rate = moretimes[common['Screen']]
+        elif common['Screen'] == '1296' and bkg.width < 1558:
+            rate = moretimes[common['Screen']]       
+        elif common['Screen'] == '900'  and bkg.height < 1115:  ##dd in this case 2:3
+            rate = moretimes[common['Screen']]    
+        else:   
+            rate = screentime[common['Screen']]  ## 16:9 format 
+       
+        if self.canvas.openPlayFile in ('snakes', 'bats', 'abstract'): 
+            fact = bkg.factor  ## should be 1.0 or default
+        else:                           
+            fact = self.bkgWorks.restoreFromTrackers(bkg, 'setscr') 
+                                                                                                                              
+        if bkg.direction == 'left': 
+            return self.bkgWorks.left(path, bkg, rate, which, fact)                    
+        elif bkg.direction == 'right':
+            return self.bkgWorks.right(path, bkg, rate, which, fact) 
+        elif bkg.direction == 'vertical':
+            return self.bkgWorks.vertical(path, bkg, rate, which, fact)   
+   
+### --------------------------------------------------------  
+    def updateXY(self, pos):
+        dragX = pos.x() - self.dragCnt.x()
+        dragY = pos.y() - self.dragCnt.y()      
+        self.x = self.initX + dragX
+        self.y = self.initY + dragY
+        
+    def scaleThis(self, key):
+        self.setOrigin()
+        if key == '>':
+            scale = .01
+        elif key == '<':
+            scale = -.01
+        self.scale += scale
+        self.setScale(self.scale)
+    
+    def setPath(self, bkg):     
+        try:  
+            node = Anime.Node(bkg)       
+            return QPropertyAnimation(node, b'pos') 
+        except RuntimeError:
+            return None
+                       
+    def delete(self):  ## also called by widget
+        self.bkgMaker.deleteBkg(self)
+   
+    def setMirrored(self, bool):
+        self.flopped = bool  
+        if not self.dots.Vertical:
+            self.setPixmap(QPixmap.fromImage(self.imgFile.mirrored(
+                # horizontally=self.flopped, vertically=False)))  ## pyside6
+                horizontal=self.flopped, vertical=False)))
+        self.setTransformationMode(Qt.TransformationMode.SmoothTransformation)
+
+    def setOrigin(self):  
+        b = self.boundingRect()
+        op = QPointF(b.width()/2, b.height()/2)
+        self.setTransformOriginPoint(op)
+        self.setTransformationMode(Qt.TransformationMode.SmoothTransformation)
+                                                          
+### ---------------------- dotsBkgItem ---------------------
+
+
+        
---
+++ src/dotsShadowWorks.py
@@ -0,0 +1,292 @@
+
+import math
+
+from PyQt6.QtCore       import Qt, QPointF, QRectF, QTimer
+from PyQt6.QtGui        import QColor, QPen, QPolygonF
+from PyQt6.QtWidgets    import QGraphicsEllipseItem, QGraphicsPolygonItem
+    
+from dotsShared         import common    
+from dotsSideGig        import distance, getCrop
+   
+PathStr = ['topLeft','topRight','botRight','botLeft'] 
+V = common['V']  ## the diameter of a pointItem, same as in ShadowWidget       
+                                            
+### -------------------- dotsShadowWorks -------------------
+''' classes: Works and PointItem '''
+### --------------------------------------------------------
+class Works:  ## small functions that were in ShadowMaker
+### --------------------------------------------------------
+    def __init__(self, parent):
+        super().__init__()
+ 
+        self.maker = parent
+        self.scene = self.maker.scene
+        
+### --------------------------------------------------------
+    def resetSliders(self): 
+        self.maker.widget.opacitySlider.setValue(int(self.maker.alpha*100))
+        self.maker.widget.opacityValue.setText('{0:.2f}'.format((self.maker.alpha)))
+        
+        self.maker.widget.scaleSlider.setValue(int(self.maker.scalor*100))
+        self.maker.widget.scaleValue.setText('{0:.2f}'.format(self.maker.scalor))
+        
+        self.maker.widget.rotaryDial.setValue(int(self.maker.rotate))
+        self.maker.widget.rotateValue.setText('{:3d}'.format(int(self.maker.rotate)))
+         
+    def closeWidget(self):
+        if self.maker.widget != None:
+            self.maker.widget.close()     
+        self.maker.widget = None  
+        
+    def deleteShadow(self): 
+        if self.maker.shadow != None:
+            self.cleanUpShadow()
+        self.maker.shadow = None
+                                                      
+    def cleanUpShadow(self):  
+        self.closeWidget()
+        self.deletePoints()  
+        self.deleteOutline()
+        self.removeShadow()
+         
+    def removeShadow(self):
+        if self.maker.shadow != None:
+            self.scene.removeItem(self.maker.shadow)
+            self.maker.linked = False
+            self.maker.shadow = None
+    
+    def addPointsToScene(self):
+        for p in self.maker.points:   
+            if self.maker.isHidden:
+                p.hide()                              
+            self.scene.addItem(p)
+                             
+    def deletePoints(self):  ## the path stays
+        for p in self.maker.points:  
+            self.scene.removeItem(p) 
+            del p
+        self.maker.points = [] 
+ 
+    def updateOutline(self): 
+        self.deleteOutline()
+        self.maker.outline = QGraphicsPolygonItem(self.makeOutline()) 
+        self.maker.outline.setPen(QPen(QColor('lime'), 2, Qt.PenStyle.DotLine))
+        self.maker.outline.setZValue(common['outline'])       
+        if self.maker.linked == True: ## used throughout shadow and updateShadow
+            self.hideOutline()      
+        self.scene.addItem(self.maker.outline)
+
+    def deleteOutline(self): 
+        if self.maker.outline != None:
+            self.scene.removeItem(self.maker.outline)
+        self.maker.outline = None
+                      
+    def makeOutline(self):  
+        outline = QPolygonF()   
+        for p in self.maker.path:  
+            outline.append(QPointF(p))
+        return outline 
+                              
+    def hidePoints(self, hide=True): 
+        for p in self.maker.points:       
+            p.hide() if hide == True else p.show()  
+                              
+    def hideOutline(self):
+        if self.maker.outline != None:
+            self.maker.outline.hide()
+            self.hidePoints()  ## default True       
+            
+    def showOutline(self):
+        if self.maker.outline != None:
+            self.maker.outline.setVisible(True)
+            self.maker.updatePath(self.maker.shadow.save) 
+            self.updateOutline()
+            self.maker.outline.show()
+            self.hidePoints(False)      
+   
+    def toggleOutline(self):
+        if self.maker.outline != None:
+            if self.maker.outline.isVisible() == True:
+                self.hideOutline()  
+            elif self.maker.outline.isVisible() == False:
+                self.showOutline()            
+                                                                   
+    def rotateShadow(self, val): 
+        inc = (val - self.maker.rotate)
+        self.rotateScale(0, -inc)
+        self.maker.rotate = val
+      
+    def scaleShadow(self, val): 
+        per = (val - self.maker.scalor) / self.maker.scalor    
+        self.rotateScale(per, 0)
+        self.maker.scalor = val
+                                                                                                             
+    def pixWidthHeight(self):
+        b = self.maker.pixitem.boundingRect()
+        return self.maker.pixitem.fileName, b.width(), b.height() 
+   
+### --------------------------------------------------------  
+    def flip(self):          
+        self.deleteOutline() 
+                        
+        x, t, b = self.maker.path[0].x(), self.maker.path[0].y(), self.maker.path[3].y()
+        y = b + (b - t)
+        self.maker.path[0] = QPointF(x,y)  
+        
+        x1, t, b = self.maker.path[1].x(), self.maker.path[1].y(), self.maker.path[2].y()
+        y1 = b + (b - t)
+        self.maker.path[1] = QPointF(x1, y1) 
+           
+        self.maker.addPoints()
+        self.maker.updatePoints(0, x, y)     
+        self.maker.updatePoints(1, x1, y1) 
+          
+        QTimer.singleShot(100, self.maker.updateShadow)
+        
+    def flopPath(self):                                               
+        x0, y0 = self.maker.path[0].x(), self.maker.path[0].y()
+        x1, y1 = self.maker.path[1].x(), self.maker.path[1].y()
+        d = x1 - x0
+
+        self.maker.path[0] = QPointF(x1 - d, y1)
+        self.maker.path[1] = QPointF(x0 + d, y0)
+
+        x2, y2 = self.maker.path[2].x(), self.maker.path[2].y()
+        x3, y3 = self.maker.path[3].x(), self.maker.path[3].y()
+        d = x3 - x2
+
+        self.maker.path[2] = QPointF(x3 - d,y3)
+        self.maker.path[3] = QPointF(x2 + d, y2)
+                 
+        self.maker.addPoints() 
+        x, y = self.maker.path[0].x(),  self.maker.path[0].y()
+        self.maker.updatePoints(0, x, y)    
+        
+        x, y = self.maker.path[1].x(),  self.maker.path[1].y()
+        self.maker.updatePoints(1, x, y)
+        
+### --------------------------------------------------------            
+    def rotateScale(self, per, inc):  ## uses path rather than pts
+        self.maker.shadow.setOriginPt() 
+        
+        x, y, w, h = getCrop(self.maker.path)   ## uses getCrop 
+        centerX, centerY = x + w/2, y + h/2
+          
+        self.maker.shadow.setTransformOriginPoint(centerX, centerY)
+        self.maker.shadow.setTransformationMode(Qt.TransformationMode.SmoothTransformation)
+           
+        for i in  [0,1,2,3]: ## range(4):  ## if you don't get 4 points...  
+            dist = distance(self.maker.path[i].x(), centerX, self.maker.path[i].y(), centerY) 
+              
+            xdist, ydist = dist, dist      
+            xdist = dist + (dist * per)              
+            ydist = xdist
+           
+            deltaX = self.maker.path[i].x() - centerX
+            deltaY = self.maker.path[i].y() - centerY
+
+            angle = math.degrees(math.atan2(deltaX, deltaY))
+            angle = angle + math.ceil( angle / 360) * 360
+
+            plotX = centerX + xdist * math.sin(math.radians(angle + inc))
+            plotY = centerY + ydist * math.cos(math.radians(angle + inc))
+           
+            self.maker.path[i] = QPointF(plotX, plotY)
+            self.maker.updatePoints(i, plotX, plotY)
+                 
+        self.maker.updateShadow()  
+           
+### --------------------------------------------------------
+class PointItem(QGraphicsEllipseItem): 
+### --------------------------------------------------------
+    def __init__(self, pt, ptStr, parent):
+        super().__init__()
+
+        self.maker = parent
+        self.maker.path  = self.maker.path   
+        self.ptStr = ptStr
+        
+        self.type = 'point'
+        self.fileName = 'point'
+        
+        self.tag = 'point'  
+        self.dragCnt = 0
+                
+        ## -V*.5 so it's centered on the path 
+        self.x = pt.x()-V*.5
+        self.y = pt.y()-V*.5
+        
+        self.setZValue(common['points'])      
+        self.setRect(self.x, self.y, V, V)  
+        
+        self.setPen(QPen(QColor('gray'), 1))
+        
+        if self.ptStr in ('topLeft','topRight'):
+            self.setBrush(QColor('yellow'))
+        else:
+            self.setBrush(QColor('lime'))
+            
+        self.setAcceptHoverEvents(True)
+                        
+ ### --------------------------------------------------------
+    def hoverEnterEvent(self, e):
+        p = self.rect()      
+        if p.width() < V*1.5:
+            self.setRect(QRectF(p.x(), p.y(), V*1.5, V*1.5))
+        e.accept()
+        
+    def hoverLeaveEvent(self, e):  
+        p = self.rect()
+        self.setRect(QRectF(p.x(), p.y(), V, V))
+        e.accept()
+
+### --------------------------------------------------------
+    def mousePressEvent(self, e):  
+        e.accept() 
+        
+    def mouseMoveEvent(self, e):
+        self.dragCnt += 1
+        if self.dragCnt % 5 == 0:            
+            self.moveIt(e.pos()) 
+            self.maker.updateShadow()             
+        e.accept()
+                       
+    def mouseReleaseEvent(self, e):
+        self.moveIt(e.pos())
+        self.maker.updateShadow()
+        e.accept()
+                                        
+    def moveIt(self, e):     
+        pos = self.mapToScene(e)
+        x, y = pos.x(), pos.y()
+        self.setRect(x-V*.5, y-V*.5, V,V)  ## set current point    
+        if self.ptStr == 'topLeft':  ## push right
+            self.topLeft(x, y)
+        elif self.ptStr == 'botLeft':    
+            self.botLeft(x, y)
+        else:  
+            i = PathStr.index(self.ptStr)  ## move only 
+            self.maker.path[i] = QPointF(x,y)                  
+        
+    def topLeft(self, x, y):
+        w, y1 = self.current(0,1)            
+        self.maker.path[0] = QPointF(x,y) 
+        self.maker.path[1] = QPointF(x+w,y1)
+        self.maker.topRight.setRect(x+(w-V*.5), y1-V*.5, V,V)  
+        
+    def botLeft(self, x, y):
+        w, y1 = self.current(3,2)            
+        self.maker.path[3] = QPointF(x,y) 
+        self.maker.path[2] = QPointF(x+w,y1)
+        self.maker.botRight.setRect(x+(w-V*.5), y1-V*.5, V,V)          
+        
+    def current(self,a,b):
+        l = self.maker.path[a].x()
+        r = self.maker.path[b].x()
+        return r - l,  self.maker.path[b].y()
+         
+### -------------------- dotsShadowWorks -------------------
+
+
+
+
